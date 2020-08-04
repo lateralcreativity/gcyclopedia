14f328b5dc4517ead1d8ca57e3878307086f841d
lateralcreativity <lateralcreativity@outlook.com>
2020-08-03 19:53:39 -0500

    ?

--- src/utils/igdbwrapper.js
+++ src/utils/igdbwrapper.js
@@ -1,7 +1,7 @@
 const igdb = require('igdb-api-node').default;
 const client = igdb(`d62a1813a58d9c855b6a319db2ed0703`, {baseURL: 'https://cors-anywhere.herokuapp.com/https://api-v3.igdb.com'})
 
-async function igdbGetGame(title) {
+export async function igdbGetGame(title) {
     try {
         const response = await client
             .fields('name,cover.image_id')
@@ -16,7 +16,7 @@ async function igdbGetGame(title) {
     }
 }
 
-async function igdbGetGameDetails(id) {
+export async function igdbGetGameDetails(id) {
     try {
         const response = await client
         .fields('name,cover.image_id,summary,genres.name,release_dates.human,videos.video_id')
@@ -28,9 +28,4 @@ async function igdbGetGameDetails(id) {
     catch (err) {
         return err
     }
-}
-
-module.exports = {
-    igdbGetGame: igdbGetGame,
-    igdbGetGameDetails: igdbGetGameDetails
 }
